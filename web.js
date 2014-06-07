1e780bde9725ae34aae5cfa93dd2ef84e98b7da6
lalumbres <alumbrel@0136M306075LM.local>
2014-06-07 00:37:42 -0400

    fixed magic 8 ball

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var cmds = [".google", ".scores", ".ud"]
 var bodyParser = require("body-parser");
 var express = require("express");
 var logfmt = require("logfmt");
-var http = require('http');
+var request = require('request');
 var app = express();
 
 app.use(logfmt.requestLogger());
@@ -16,23 +16,15 @@ app.get('/', function(req, res) {
 
 // Handle POST request
 app.post('/receiver', function(req, res) {
-	console.log(req.body);
 	var fromUser = req.body.name;
 	var message = req.body.text;
-	console.log(fromUser)
-	console.log(message)
 	if (message) {
-		console.log(message);
 		msgTokens = message.split(" ");
 		if (msgTokens && msgTokens.length > 1) {
-			console.log(msgTokens);
 			var bot = msgTokens[0];
-			console.log(bot);
 			if (bot.toLowerCase() == botName) {
 				var cmd = msgTokens[1];
 				var lastToken = msgTokens[msgTokens.length - 1];
-				console.log(cmd);
-				console.log(lastToken);
 				// Google search
 				if (cmd.toLowerCase() == cmds[0]) { 
 					
@@ -77,57 +69,7 @@ app.post('/receiver', function(req, res) {
 						randomMessage = messages[randomnumber];
 						answer = fromUser + ", " + randomMessage;
 						
-						var botMessage = {
-							bot_id: "6700b3625fa11e760d1a66460b",
-							text: answer
-						};
-
-						var dataString = JSON.stringify(botMessage);
-						
-						var headers = {
-							'Content-Type': 'application/json',
-							'Content-Length': dataString.length
-						};
-						
-						var options = {
-							host: 'api.groupme.com',
-							path: '/v3/bots/post',
-							method: 'POST',
-							headers: headers
-						};
-						
-						// Setup the request.  The options parameter is
-						// the object we defined above.
-						var request = http.request(options, function(response) {
-							response.setEncoding('utf-8');
-							var responseString = '';
-							
-							response.on('data', function(data) {
-								responseString += data;
-							});
-							
-							response.on('end', function() {
-								var resultObject = JSON.parse(responseString);
-							});
-						});
-
-						request.on('error', function(e) {
-						  // TODO: handle error.
-						});
-
-						request.write(dataString);
-						request.end();
-						
-						/*
-						$.ajax({
-							type: "POST",
-							url: "https://api.groupme.com/v3/bots/post",
-							dataType: 'json',
-							async: false,
-							data: '{"text" : "'+answer+'", "bot_id" : "6700b3625fa11e760d1a66460b"}',
-							success: function(){}
-						});
-						*/
+						request.post('https://api.groupme.com/v3/bots/post', {form:{bot_id: '6700b3625fa11e760d1a66460b',text: answer}});
 					}
 				}
 			}
