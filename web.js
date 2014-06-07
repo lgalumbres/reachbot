639f9c31e9b66244e766ec1128c65650d88e18bd
lalumbres <alumbrel@01sic305803l.corp.espn.pvt>
2014-06-07 16:43:30 -0400

    blah

--- web.js
+++ web.js
@@ -24,141 +24,146 @@ app.post('/receiver', function(req, res) {
 	var botId = (groupId == "8592658" ? "6700b3625fa11e760d1a66460b" : "1b0a65dc0963428c4d1946d735");
 	
 	if (message) {
-		msgTokens = message.split(" ");
-		if (msgTokens && msgTokens.length > 1) {
-			var bot = msgTokens[0];
-			if (bot.toLowerCase() == botName) {
-				var cmd = msgTokens[1];
-				var lastToken = msgTokens[msgTokens.length - 1];
-				// Google search
-				if (cmd.toLowerCase() == cmds[0]) { 
-					
-				} 
-				// Scores query
-				else if (cmd.toLowerCase() == cmds[1]) {
-					var answer = "";
-					var sport = msgTokens[2];
-					var host = "http://api.espn.com";
-					var queryParams = "?advance=true&apiKey=7zx98fmr2e5mwmbgjezfnxeu";
-					var scoresPaths = new HashMap();
-					scoresPaths.set("nhl","/v1/sports/hockey/nhl/events");
-					scoresPaths.set("mlb","/v1/sports/baseball/mlb/events");
-					scoresPaths.set("nfl","/v1/sports/football/nfl/events");
-					scoresPaths.set("ncaaf","/v1/sports/football/college-football/events");
-					scoresPaths.set("nba","/v1/sports/basketball/nba/events");
-					scoresPaths.set("ncaab","/v1/sports/basketball/mens-college-basketball/events");
-					scoresPaths.set("wnba","/v1/sports/basketball/wnba/events");
-					scoresPaths.set("ncaaw","/v1/sports/basketball/womens-college-basketball/events");
-					
-					var path = scoresPaths.get(sport);
-					if (path) {
-						var url = host + path + queryParams;
-						request(url, function (error, response, body) {
-							if (!error && response.statusCode == 200) {
-								jsonObj = JSON.parse(body);
-								if (jsonObj) {
-									var events = jsonObj.sports[0].leagues[0].events;
-									if (events && events.length > 0) {
-										var addComma = false;
-										for (var i = 0; i < events.length; i++) {
-											var gameInfo = "";
-											var event = events[i].competitions[0];
-											var status = event.status;
-											var homeCompetitor = event.competitors[0];
-											var awayCompetitor = event.competitors[1];
-											if (status.state == "pre") {
-												var detail = status.shortDetail;
-												if (event.timeValid) {
-													var date = new Date(status.shortDetail);
-													dateStr = date.toFormat("DDD MM/DD H:MI PP");
-													gameInfo = dateStr + " - " + awayCompetitor.team.abbreviation + " @ " + homeCompetitor.team.abbreviation;
+		if (message.toLowerCase() == "i love you reachbot") {
+			request.post('https://api.groupme.com/v3/bots/post', {form:{bot_id: botId,text: "I love you too "+fromUser+"?"}});
+		}
+		else {
+			msgTokens = message.split(" ");
+			if (msgTokens && msgTokens.length > 1) {
+				var bot = msgTokens[0];
+				if (bot.toLowerCase() == botName) {
+					var cmd = msgTokens[1];
+					var lastToken = msgTokens[msgTokens.length - 1];
+					// Google search
+					if (cmd.toLowerCase() == cmds[0]) { 
+						
+					} 
+					// Scores query
+					else if (cmd.toLowerCase() == cmds[1]) {
+						var answer = "";
+						var sport = msgTokens[2];
+						var host = "http://api.espn.com";
+						var queryParams = "?advance=true&apiKey=7zx98fmr2e5mwmbgjezfnxeu";
+						var scoresPaths = new HashMap();
+						scoresPaths.set("nhl","/v1/sports/hockey/nhl/events");
+						scoresPaths.set("mlb","/v1/sports/baseball/mlb/events");
+						scoresPaths.set("nfl","/v1/sports/football/nfl/events");
+						scoresPaths.set("ncaaf","/v1/sports/football/college-football/events");
+						scoresPaths.set("nba","/v1/sports/basketball/nba/events");
+						scoresPaths.set("ncaab","/v1/sports/basketball/mens-college-basketball/events");
+						scoresPaths.set("wnba","/v1/sports/basketball/wnba/events");
+						scoresPaths.set("ncaaw","/v1/sports/basketball/womens-college-basketball/events");
+						
+						var path = scoresPaths.get(sport);
+						if (path) {
+							var url = host + path + queryParams;
+							request(url, function (error, response, body) {
+								if (!error && response.statusCode == 200) {
+									jsonObj = JSON.parse(body);
+									if (jsonObj) {
+										var events = jsonObj.sports[0].leagues[0].events;
+										if (events && events.length > 0) {
+											var addComma = false;
+											for (var i = 0; i < events.length; i++) {
+												var gameInfo = "";
+												var event = events[i].competitions[0];
+												var status = event.status;
+												var homeCompetitor = event.competitors[0];
+												var awayCompetitor = event.competitors[1];
+												if (status.state == "pre") {
+													var detail = status.shortDetail;
+													if (event.timeValid) {
+														var date = new Date(status.shortDetail);
+														dateStr = date.toFormat("DDD MM/DD H:MI PP");
+														gameInfo = dateStr + " - " + awayCompetitor.team.abbreviation + " @ " + homeCompetitor.team.abbreviation;
+													}
 												}
+												else if (status.state == "in" || status.state == "post") {
+													gameInfo = status.shortDetail + " - " + awayCompetitor.team.abbreviation + " (" + awayCompetitor.score + ") @ " + homeCompetitor.team.abbreviation + " (" + homeCompetitor.score + ")";
+												}
+												answer = answer + gameInfo + (i == events.length - 1 ? "" : ", ");
 											}
-											else if (status.state == "in" || status.state == "post") {
-												gameInfo = status.shortDetail + " - " + awayCompetitor.team.abbreviation + " (" + awayCompetitor.score + ") @ " + homeCompetitor.team.abbreviation + " (" + homeCompetitor.score + ")";
-											}
-											answer = answer + gameInfo + (i == events.length - 1 ? "" : ", ");
+										}
+										else {
+											answer = fromUser + ", no games scheduled right now.";
 										}
 									}
 									else {
-										answer = fromUser + ", no games scheduled right now.";
+										answer = fromUser + ", something went wrong.... I need to go to the bathroom.";
 									}
+									request.post('https://api.groupme.com/v3/bots/post', {form:{bot_id: botId,text: answer}});
 								}
-								else {
-									answer = fromUser + ", something went wrong.... I need to go to the bathroom.";
-								}
-								request.post('https://api.groupme.com/v3/bots/post', {form:{bot_id: botId,text: answer}});
-							}
-						});
-					} else {
-						answer = fromUser + ", what's " + term +"? I don't know the schedule for that shit."; 
-						request.post('https://api.groupme.com/v3/bots/post', {form:{bot_id: botId,text: answer}});
+							});
+						} else {
+							answer = fromUser + ", what's " + term +"? I don't know the schedule for that shit."; 
+							request.post('https://api.groupme.com/v3/bots/post', {form:{bot_id: botId,text: answer}});
+						}
 					}
-				}
-				// Urban Dictionary search
-				else if (cmd.toLowerCase() == cmds[2]) {
-					var term = msgTokens[2];
-					var url = "http://api.urbandictionary.com/v0/define?term="+term
+					// Urban Dictionary search
+					else if (cmd.toLowerCase() == cmds[2]) {
+						var term = msgTokens[2];
+						var url = "http://api.urbandictionary.com/v0/define?term="+term
 
-					request({
-						url: url,
-						json: true
-					}, function (error, response, body) {
-					    if (!error && response.statusCode === 200) {
-					    	listItems = body.list;
-					    	answer = "";
-					    	if (listItems && listItems.length > 0) {
-					    		listItem = listItems[0];
-					    		answer = fromUser + ", " + term + " means '" + listItem.definition + "'.";
-					    	}
-					    	else {
-					    		answer = fromUser + ", I couldnt find a definition on " + term +"."; 
-					    	}
-					    	request.post('https://api.groupme.com/v3/bots/post', {form:{bot_id: botId,text: answer}});
-					    }
-					});
-				}
-				// Magic 8 Ball
-				else {
-					if (lastToken.indexOf("?") > -1) {
-						var messages = new Array();
-						messages[0] = "no.";
-						messages[1] = "not today.";
-						messages[2] = "it is decidedly so.";
-						messages[3] = "without a doubt.";
-						messages[4] = "yes definitely.";
-						messages[5] = "you may rely on it.";
-						messages[6] = "as I see it yes.";
-						messages[7] = "most likely.";
-						messages[8] = "outlook good.";
-						messages[10] = "signs point to yes.";
-						messages[11] = "reply hazy try again.";
-						messages[12] = "ask again later.";
-						messages[13] = "better not tell you now.";
-						messages[14] = "cannot predict now.";
-						messages[15] = "concentrate and ask again.";
-						messages[16] = "don't count on it.";
-						messages[17] = "my reply is no.";
-						messages[18] = "my sources say no.";
-						messages[19] = "outlook not so good.";
-						messages[20] = "very doubtful.";
-						messages[21] = "fuck John Cho!";
-						messages[22] = "yaasss!";
-						messages[23] = "one less lonely negroll....";
-						
-						var randomnumber = Math.floor(Math.random() * 23);
-						randomMessage = messages[randomnumber];
-						answer = fromUser + ", " + randomMessage;
-						
-						request.post('https://api.groupme.com/v3/bots/post', {form:{bot_id: botId,text: answer}});
+						request({
+							url: url,
+							json: true
+						}, function (error, response, body) {
+						    if (!error && response.statusCode === 200) {
+						    	listItems = body.list;
+						    	answer = "";
+						    	if (listItems && listItems.length > 0) {
+						    		listItem = listItems[0];
+						    		answer = fromUser + ", " + term + " means '" + listItem.definition + "'.";
+						    	}
+						    	else {
+						    		answer = fromUser + ", I couldnt find a definition on " + term +"."; 
+						    	}
+						    	request.post('https://api.groupme.com/v3/bots/post', {form:{bot_id: botId,text: answer}});
+						    }
+						});
+					}
+					// Magic 8 Ball
+					else {
+						if (lastToken.indexOf("?") > -1) {
+							var messages = new Array();
+							messages[0] = "no.";
+							messages[1] = "not today.";
+							messages[2] = "it is decidedly so.";
+							messages[3] = "without a doubt.";
+							messages[4] = "yes definitely.";
+							messages[5] = "you may rely on it.";
+							messages[6] = "as I see it yes.";
+							messages[7] = "most likely.";
+							messages[8] = "outlook good.";
+							messages[10] = "signs point to yes.";
+							messages[11] = "reply hazy try again.";
+							messages[12] = "ask again later.";
+							messages[13] = "better not tell you now.";
+							messages[14] = "cannot predict now.";
+							messages[15] = "concentrate and ask again.";
+							messages[16] = "don't count on it.";
+							messages[17] = "my reply is no.";
+							messages[18] = "my sources say no.";
+							messages[19] = "outlook not so good.";
+							messages[20] = "very doubtful.";
+							messages[21] = "fuck John Cho!";
+							messages[22] = "yaasss!";
+							messages[23] = "one less lonely negroll....";
+							
+							var randomnumber = Math.floor(Math.random() * 23);
+							randomMessage = messages[randomnumber];
+							answer = fromUser + ", " + randomMessage;
+							
+							request.post('https://api.groupme.com/v3/bots/post', {form:{bot_id: botId,text: answer}});
+						}
 					}
 				}
 			}
-		}
-		else if (msgTokens && msgTokens.length == 1) {
-			var bot = msgTokens[0];
-			if (bot.toLowerCase() == botName) {
-				request.post('https://api.groupme.com/v3/bots/post', {form:{bot_id: botId,text: "Yeah, what's up "+fromUser+"?"}});
+			else if (msgTokens && msgTokens.length == 1) {
+				var bot = msgTokens[0];
+				if (bot.toLowerCase() == botName) {
+					request.post('https://api.groupme.com/v3/bots/post', {form:{bot_id: botId,text: "Yeah, what's up "+fromUser+"?"}});
+				}
 			}
 		}
 	}
